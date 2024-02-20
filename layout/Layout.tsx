1968a2b0770a4110d35f83ab9fd8d3013feeb835
Polina Limonova <polina98_08@mail.ru>
2024-02-20 12:23:33 +0300

    Skip link

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, KeyboardEvent, useRef, useState } from "react";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { LayoutProps } from "./Layout.types";
@@ -6,13 +6,37 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import styles from "./Layout.module.css";
 import { AppContextProvider, IAppContext } from "../context/app.context";
 import { Up } from "../components";
+import cn from "classnames";
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const [isDisplayed, setIsDisplayed] = useState<boolean>(false);
+  const bodyRef = useRef<HTMLDivElement>(null);
+
+  const skipContentHandle = (e: KeyboardEvent) => {
+    if (e.code == "Space" || e.code == "Enter") {
+      e.preventDefault();
+      bodyRef.current?.focus();
+    }
+    setIsDisplayed(false);
+  };
+
   return (
     <div className={styles.wrapper}>
+      <a
+        onFocus={() => setIsDisplayed(true)}
+        tabIndex={1}
+        className={cn(styles.skiplink, {
+          [styles.displayed]: isDisplayed,
+        })}
+        onKeyDown={skipContentHandle}
+      >
+        К содержанию
+      </a>
       <Header className={styles.header} />
       <Sidebar className={styles.sidebar} />
-      <div className={styles.body}>{children}</div>
+      <div className={styles.body} ref={bodyRef} tabIndex={0}>
+        {children}
+      </div>
       <Footer className={styles.footer} />
       <Up />
     </div>
